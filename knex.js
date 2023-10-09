a6edefab7cb28be66cdba0b1842fef57c9b874f9
Farroussa <104143733+Farroussa@users.noreply.github.com>
2023-10-08 21:50:46 -0700

    second commit

--- knex.js
+++ knex.js
@@ -77,3 +77,7 @@ app.delete('/users/:id', async (req, res) => {
     }
 });
 
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
